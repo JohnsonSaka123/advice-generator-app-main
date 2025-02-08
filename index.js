2fdb4234e391f6862a18167ee05599d8fcf2d72c
Johnson <sakajohnson1@gmail.com>
2025-02-08 11:33:49 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+// Interacting with the DOM
+const dice = document.querySelector(".card__dice");
+const id = document.querySelector(".card__number");
+const advice = document.querySelector(".card__quote");
+
+// API request 
+function loadQuote(){
+  fetch("https://api.adviceslip.com/advice")
+    .then((response) => {
+      return response.json();
+    })
+  .then((response) => {
+    id.innerHTML = `Advice #${response.slip.id}`;
+    advice.innerHTML = `&#8220${response.slip.advice}&#8221`;
+  });
+}
+
+dice.addEventListener("click" , loadQuote);
